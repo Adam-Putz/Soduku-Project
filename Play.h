1fa232708533f528c3df0e46ac2e19572f22b837
Adam Putz <PutzAM11@uww.edu>
2022-04-13 11:04:05 -0500

    commit on 'Main'

--- Play.h
+++ Play.h
@@ -1,9 +1,45 @@
 #include"Move.h"
 class Play
 {
+    private:
+    int** board;
     public:
+    Play()
+    {
+    }
     void run()
+    {
+        //Adam
+    }
+    int** generateBoard(char difficulty)
+    {
+        //Coles
+    }
+    void updateBoard(Move playerMove)
+    {
+        //Adam
+    }
+    bool isValidMove()
+    {
+        //Brian
+    }
+    void printGamePlay()
+    {
+        //Brian
+    }
+    void menu()
+    {
+        //Brian
+    }
+    Move getPlayerMove(int x, int y, int num)
     {
         
     }
+    Move undo()
+    {
+
+    }
+    Move redo()
+
+
 };
